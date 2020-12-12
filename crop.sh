2fa732b7530aa107f98cc2ed4ebec4a3878a8705
Will <rwd1@hotmail.co.uk>
2020-12-12 20:58:01 +0000

    Switch to h.265 and changed settings to production

--- crop.sh
+++ crop.sh
@@ -6,9 +6,9 @@ for input in "Input"/*
 do
     echo "$input"
     output=`basename $input`
-    crop=`ffmpeg -ss 60 -i $input -vframes 10 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1`
+    crop=`ffmpeg -ss 60 -i $input -vframes 24 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1`
     echo "Detected crop format: $crop"
-    ffmpeg -i $input -t 30 -vf $crop -map 0:v -map 0:a -map 0:s -c:s dvdsub \
-    -c:v libx264 -preset fast -crf 22 -c:a \
+    ffmpeg -i $input -vf $crop -map 0:v -map 0:a -map 0:s -c:s dvdsub \
+    -c:v libx265 -preset slow -crf 22 -c:a \
     copy Output/$output
 done
