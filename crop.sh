bb064e1dd3d690bdf93ddae4571c640c046d690b
Will <rwd1@hotmail.co.uk>
2020-12-13 10:00:40 +0000

    Moves processed files

--- crop.sh
+++ crop.sh
@@ -2,13 +2,17 @@
 
 echo "Welcome to autcrop"
 
-for input in "Input"/*
+set workingDir=""
+
+for input in "$workingDir/Input"/*
 do
     echo "$input"
-    output=`basename $input`
+    fileName=`basename $input`
     crop=`ffmpeg -ss 60 -i $input -vframes 24 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1`
     echo "Detected crop format: $crop"
     ffmpeg -i $input -vf $crop -map 0:v -map 0:a -map 0:s -c:s dvdsub \
     -c:v libx265 -preset slow -crf 22 -c:a \
-    copy Output/$output
+    copy $workingDir/Output/$fileName
+
+    mv $input $workingDir/Processed
 done
